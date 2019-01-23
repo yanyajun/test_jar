842cf1376e89c52bc3efa26ea2f51465a349891e
yanyajun <516288725@qq.com>
2019-01-23 14:53:26 +0800

    commit code

---
+++ app/src/main/java/com/example/yyj/test_jar/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.yyj.test_jar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+//import com.example.testlib.TestUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        TestUtil testUtil = new TestUtil();
+//        int ret = testUtil.add(1,2);
+//        Log.e("===>", "lib ret: " + ret);
+    }
+}
---
+++ testlib/src/main/java/com/example/testlib/TestUtil.java
@@ -0,0 +1,12 @@
+package com.example.testlib;
+
+/**
+ * Created by yyj on 2018/10/24.
+ */
+
+public class TestUtil {
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+}
